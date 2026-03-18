551a7f56864429dd8da38c0793c3e9b4d29fea95
SenczuckCode <lucas_senczuck@estudante.sesisenai.org.br>
2026-03-18 09:34:24 -0300

    ADD- Versão 0.1 calculadora

--- Calculadora_java/script.js
+++ Calculadora_java/script.js
@@ -3,12 +3,12 @@ function adicionar(valor) {
 }
 
 function limpar() {
-    document.getElementById('visor').value = '';
-}
-
-function apagar() {
     var visor = document.getElementById('visor');
     visor.value = visor.value.slice(0, -1);
+}
+
+function excluir() {
+    document.getElementById('visor').value = '';
 }
 
 function calcular() {
